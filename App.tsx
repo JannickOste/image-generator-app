0f3cd0a2b15135775c2d0c2c9b7427da80602eee
Kasper B <kasperbosteels@hotmail.com>
2022-11-07 15:25:56 +0100

    fixed not closing navigator tag

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
-import { StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Create,Home,Images} from "./Components/Stack.navigator";
+import { HomePage, CreatePage, ImagesPage } from "./Components/Stack.navigator";
 export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <>
       <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home}/>
-        <Tab.Screen name="Create" component={Create}/>
-        <Tab.Screen name="Images" component={Images}/>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomePage} />
+          <Tab.Screen name="Create" component={CreatePage} />
+          <Tab.Screen name="Images" component={ImagesPage} />
+        </Tab.Navigator>
       </NavigationContainer>
     </>
   );
