572823d33d19a57cbfc9847e745af847a05eecfd
Kasper B <kasperbosteels@hotmail.com>
2022-11-07 15:11:15 +0100

    added Tab navigation for main pages

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Create,Home,Images} from "./Components/Stack.navigator";
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen name="Create" component={Create}/>
+        <Tab.Screen name="Images" component={Images}/>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Components/Stack.navigator.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import {
+  useNavigation,
+  useRoute,
+  NavigationContainer,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Constants from "expo-constants";
+const Stack: any = createNativeStackNavigator();
+export const Home = () => {
+  return <></>;
+};
+export const Create = () => {
+  return <></>;
+};
+export const Images = () => {
+  return <></>;
+};
