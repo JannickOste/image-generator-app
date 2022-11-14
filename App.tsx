de6e52aca909840dca5f4ea8654afa677c4a14f8
Kasper B <kasperbosteels@hotmail.com>
2022-11-14 19:45:46 +0100

    temporarily added editimages to tab

--- App.tsx
+++ App.tsx
@@ -2,15 +2,19 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomePage, CreatePage, ImagesPage } from "./Components/Stack.navigator";
+import EditImage from "./Components/EditImage";
+
 export default function App() {
+  
   const Tab = createBottomTabNavigator();
   return (
     <>
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name="Home" component={HomePage} />
-          <Tab.Screen name="Create" component={CreatePage} />
+          <Tab.Screen name="Create" component={CreatePage}  />
           <Tab.Screen name="Images" component={ImagesPage} />
+          <Tab.Screen name="EditImage" component={EditImage} />
         </Tab.Navigator>
       </NavigationContainer>
     </>
