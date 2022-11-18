58507d4081d914cbd55de32eff1d03755b01d35f
Kasper B <kasperbosteels@hotmail.com>
2022-11-18 01:45:54 +0100

    fixed some issues more to come

--- Components/EditImage.tsx
+++ Components/EditImage.tsx
@@ -1,11 +1,19 @@
-import React,{  useRef} from 'react';
+import React,{  useRef, useState} from 'react';
 import {Button, ImageBackground, TextInput} from "react-native"
-import {Canvas,CanvasRef} from '@benjeau/react-native-draw';
+import {Canvas,CanvasRef, PathType,DrawingTool} from '@benjeau/react-native-draw';
 import { RouteProp, useRoute } from '@react-navigation/native';
 interface EditImageProps {
     uri:string
 }
 const EditImage  =(()=>{
+    const [paths,setPaths] = useState<PathType[]>([]);
+    const [prompt, setPrompt] = useState<string>("");
+    const addPath =(path:PathType[])=>{
+    //setPaths([...paths,...path])
+    }
+    const clearPaths=()=>{
+     setPaths([]);
+    }
     const route: RouteProp<any> = useRoute();
     const uri = route.params?.uri;
     const canvasRef = useRef<CanvasRef>(null);
@@ -20,16 +28,22 @@ const EditImage  =(()=>{
    <Canvas
         ref={canvasRef}
         height={500}
-        color="black"
+        color="green"
         thickness={15}
-        opacity={0.5}
+        tool={DrawingTool.Brush}
+        opacity={1}
         style={{backgroundColor:'rgba(52, 52, 52, 0.0)',}}
+        onPathsChange={(event)=>{addPath(event)}}
         
     />
     </ImageBackground>  
-    <Button title="undo" onPress={handleUndo}/>
-    <Button title="clear" onPress={handleClear}/>
+    <Button title="undo" onPress={()=>{handleUndo; console.log(paths)}}/>
+    <Button title="clear"onPress={()=>{handleClear; clearPaths;}}/>
+    {paths.length >=1 && prompt != "" ? 
+        (<Button title="Generate!" onPress={()=>{}}/>):
+        (<TextInput placeholder='A room with an blue eliphant in it.' onSubmitEditing={(event)=>setPrompt(event.nativeEvent.text)}/>)
+    }
    </>
-   
+   //input here in backend to generate AI image or modify image if that is still required.
 })
 export default EditImage;
