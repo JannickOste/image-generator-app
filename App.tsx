7a21e1888d2b3f0403ea3034811c3112c3622a3f
Kasper B <kasperbosteels@hotmail.com>
2022-12-03 23:57:58 +0100

    added basic icons

--- App.tsx
+++ App.tsx
@@ -5,16 +5,33 @@ import { CreateNavigator } from "./Components/Stack.navigator";
 import Home from "./Components/Home";
 import Images from "./Components/Images";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <>
      <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Create" component={CreateNavigator}  />
-          <Tab.Screen name="Images" component={Images} />
+          <Tab.Screen name="Home" component={Home} options={{ 
+            tabBarIcon:()=>(
+              <MaterialCommunityIcons 
+                name="home" 
+                size={26} 
+                color="black"/>),}} />
+          <Tab.Screen name="Create" component={CreateNavigator} options={{
+            tabBarIcon:()=>(
+                <MaterialCommunityIcons
+                  name="camera"
+                  size={26}
+                  color="black"/>),
+          }} />
+          <Tab.Screen name="Images" component={Images} options={{
+            tabBarIcon:()=>(
+              <MaterialCommunityIcons
+                name="image"
+                size={26}
+                color="black"/>),
+            }}/>
         </Tab.Navigator>
   </NavigationContainer>
     </>
