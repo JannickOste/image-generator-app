8b2fc81d1bdcdbe558a7f9cedd9ea78513c34da5
Kasper B <kasperbosteels@hotmail.com>
2022-12-05 14:35:31 +0100

    added more styling

--- Components/pages/Home.tsx
+++ Components/pages/Home.tsx
@@ -1,13 +1,10 @@
 
-import {View, Text, StyleSheet, Linking, Button,} from 'react-native';
-import { Link } from '@react-navigation/native';
+import {View, Text, StyleSheet, Linking,} from 'react-native';
 import * as React from 'react'
 import Constants from 'expo-constants';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 const policiesLink:string = "https://beta.openai.com/docs/usage-policies"
 
-  const handlePress =async ()=>{
-    await Linking.openURL(policiesLink);
-    }
    
 const Home = () => {
 
@@ -15,8 +12,8 @@ const Home = () => {
   <>  
     <View style={styles.container}>
       <Text style={styles.textField}>Welcome to the image-generator-app!</Text>
-      <Text style={styles.textField}>Please read the usage case policy from OpenAi first before proceeding.</Text>
-      <Link to={policiesLink} style={styles.linkfield}>OpenAi Policies</Link>
+      <Text style={styles.textField}>Please read the policies from OpenAi first before proceeding.</Text>
+      <TouchableOpacity style={styles.touchable} onPress={() => Linking.openURL('https://google.com')}><Text style={styles.touchableText}>Policies</Text></TouchableOpacity>
     </View>
   </>
   );
@@ -25,6 +22,7 @@ const styles = StyleSheet.create({
   container:{
     flex:2, 
     JustifyContent:"center", 
+    alignItems:"center",
     paddingTop:Constants.statusBarHeight,
     padding:9
   },
@@ -42,6 +40,18 @@ const styles = StyleSheet.create({
     textAlign:'center',
     textDecoration:"none",
     color:"blue"
+  },
+  touchable:{
+    backgroundColor:"rgba(0,100,100,0.7)",
+    borderRadius:7,
+    width:120,
+    height:30,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  touchableText:{
+    fontSize:20,
+    color:"#ffffff"
   }
 })
 export default Home;
