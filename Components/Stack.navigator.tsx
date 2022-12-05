2537c4b39be4fdb0e7e3d3ea136f77eff6406b42
Kasper B <kasperbosteels@hotmail.com>
2022-12-05 14:38:55 +0100

    stack navigator fix

--- Components/Stack.navigator.tsx
+++ Components/Stack.navigator.tsx
@@ -1,23 +1,28 @@
 import * as React from "react";
-export type RootStackParamList = {
-  editimage:APIImageProps
-}
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Create from "./pages/Create";
 import Home from "./pages/Home";
 import Images from "./pages/Images";
 import History from "./pages/History";
 import ImageCreator, { APIImageProps } from "./ImageCreator";
-const Stack = createNativeStackNavigator();
-  export const HomePage = () => {
+
+
+
+export type RootStackParamList = {
+  editimage:APIImageProps
+}
+export const CreateNavigator = ()=>{
+  const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator>
-        <Stack.Screen name="chooseimage" component={Create}/>
+        <Stack.Screen name="Images" component={Images}/>
         <Stack.Screen name="editimage">{() => <ImageCreator width={255} height={255}/>}</Stack.Screen>
     </Stack.Navigator>
     )
 
-}
+  }
+
 export const HomeNavigator=()=>{
   const Stack = createNativeStackNavigator();
   return (
@@ -32,13 +37,9 @@ export const ImagesNavigator=()=>{
   const Stack = createNativeStackNavigator();
   return (
         <Stack.Navigator>
-          <Stack.Screen name="Images" component={Images}/>
+          <Stack.Screen name="History" component={History}/>
           <Stack.Screen name="Home" component={Home}/>
         </Stack.Navigator>
 
   )
-}
-export const HistoryPage = () =>{
-  return (<History />)
-
 }
