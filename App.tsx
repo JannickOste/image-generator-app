4f36abfc66a92c1b3405a3a32c8e1e20d3cb6ecd
Kasper B <kasperbosteels@hotmail.com>
2022-12-05 14:38:41 +0100

    changed color of icons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { CreateNavigator } from "./Components/Stack.navigator";
+import { CreateNavigator, HomeNavigator, ImagesNavigator } from "./Components/Stack.navigator";
 import Home from "./Components/pages/Home";
 import Images from "./Components/pages/Images";
-import { HomePage, CreatePage, ImagesPage, HistoryPage } from "./Components/Stack.navigator";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 export default function App() {
@@ -13,25 +12,25 @@ export default function App() {
     <>
      <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} options={{ 
+          <Tab.Screen name="Welcome" component={HomeNavigator} options={{ 
             tabBarIcon:()=>(
               <MaterialCommunityIcons 
                 name="home" 
-                size={26} 
-                color="black"/>),}} />
+                size={30} 
+                color="rgba(0,100,100,0.7)"/>),}} />
           <Tab.Screen name="Create" component={CreateNavigator} options={{
             tabBarIcon:()=>(
                 <MaterialCommunityIcons
                   name="camera"
-                  size={26}
-                  color="black"/>),
+                  size={30}
+                  color="rgba(0,100,100,0.7)"/>),
           }} />
-          <Tab.Screen name="Images" component={Images} options={{
+          <Tab.Screen name="All images" component={ImagesNavigator} options={{
             tabBarIcon:()=>(
               <MaterialCommunityIcons
                 name="image"
-                size={26}
-                color="black"/>),
+                size={30}
+                color="rgba(0,100,100,0.7)"/>),
             }}/>
         </Tab.Navigator>
   </NavigationContainer>
