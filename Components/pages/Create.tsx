b73ab1956cf19b0126dc2f99d433e0551a9cee27
Kasper B <kasperbosteels@hotmail.com>
2022-12-19 15:03:49 +0100

    changed colors to something more easy. different svg per page

--- Components/pages/Create.tsx
+++ Components/pages/Create.tsx
@@ -11,7 +11,7 @@ import Constants from "expo-constants";
 import WavyHeader from "../wavyHeader";
 import { LinearGradient } from "expo-linear-gradient";
 import CustomHeader from "../header";
-
+const svg="M0,128L48,138.7C96,149,192,171,288,197.3C384,224,480,256,576,250.7C672,245,768,203,864,176C960,149,1056,139,1152,165.3C1248,192,1344,256,1392,288L1440,320L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
 const Create = () => {
   const [photo, setPhoto] = useState<string | null>(null);
   const [CameraPermission, requestCameraPermission] = Camera.useCameraPermissions();
@@ -50,14 +50,17 @@ const Create = () => {
       console.log(photo)
     }
   };
+
   return (
     <>
-    <WavyHeader/>
+     <WavyHeader 
+      svg={svg}
+      color="rgb(150,150,230)"/>
     <LinearGradient
       style={styles.gradient}
-      colors={["rgba(0,100,100,0.3)","rgba(0,100,100,0.7)"]}
-      start={{x:0,y:0.5}}
-      end={{x:1.3,y:1}}>
+      colors={["rgba(150,150,230,0.3)","rgba(150,100,230,0.7)"]}
+      start={{x:0,y:0.4}}
+      end={{x:2,y:1}}>
         <CustomHeader title="Select Image" />
     <SafeAreaView style={styles.viewArea}>
       <View style={styles.ImageContainer}>
--- Components/pages/History.tsx
+++ Components/pages/History.tsx
@@ -46,14 +46,17 @@ const ImageHistory = () => {
         </ScrollView>
     )
 }
+const svg="M0,128L48,112C96,96,192,64,288,80C384,96,480,160,576,176C672,192,768,160,864,122.7C960,85,1056,43,1152,53.3C1248,64,1344,128,1392,160L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
 export const History = () => ( 
     <>
-<WavyHeader/>
-<LinearGradient
-    style={styles.gradient}
-    colors={["rgba(0,100,100,0.3)","rgba(0,100,100,0.7)"]}
-    start={{x:0,y:0.5}}
-    end={{x:1.3,y:1}}>
+   <WavyHeader
+        svg={svg}
+        color="rgb(150,150,230)"/>
+    <LinearGradient
+      style={styles.gradient}
+      colors={["rgba(150,150,230,0.3)","rgba(150,100,230,0.7)"]}
+      start={{x:0,y:0.4}}
+      end={{x:2,y:1}}>
 <CustomHeader title="Image History"/>
 <ImageHistory /> 
 </LinearGradient>
