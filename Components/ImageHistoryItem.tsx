3169220745b0a2d91ac117104046bb9cabfb875d
Kasper B <kasperbosteels@hotmail.com>
2022-12-27 17:41:29 +0100

    history item styling

--- Components/ImageHistoryItem.tsx
+++ Components/ImageHistoryItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, View, Text, Image, StyleSheet } from "react-native";
+import { Button, View, Text, Image, StyleSheet, Pressable } from "react-native";
 
 export type ImageHistoryItemProps = {
     id:number;
@@ -13,14 +13,41 @@ const styles = StyleSheet.create({
         display: "flex", 
         flexDirection: "column", 
         justifyContent: "space-evenly",
+        alignItems:"center",
         margin: 5
     },
     imageContainer:{
-        marginHorizontal: "auto"
+        marginHorizontal: "auto",
+        zIndex:1
     },
     historyContainer:{
         color: "black", 
-        textAlign: "center" 
+        textAlign: "center" ,
+        backgroundColor:"rgba(0,0,0,0.2)",
+        borderBottomLeftRadius:7,
+        borderBottomRightRadius:7,
+        width:"100%",
+        zIndex:0,
+        top:-11,
+        paddingTop:15,
+        marginBottom:"5%"
+    },
+    button:{
+        backgroundColor:"rgba(200,100,200,0.7)",
+        alignItems:"center",
+        justifyContent:"center",
+        paddingVertical:12,
+        paddingHorizontal:0,
+        elevation:3,
+        width:"70%",
+        marginBottom:5
+    },
+    buttonText:{
+        fontSize:13,
+        lineHeight:21,
+        fontWeight:"bold",
+        letterSpacing:0.25,
+        color:"white"
     }
 });
 
@@ -28,12 +55,13 @@ const ImageHistoryItem = ({id, searchQuery, uri, onRemove}: ImageHistoryItemProp
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
-                <Image source={{uri: uri, width: 255, height: 255}} /> 
+                <Image source={{uri: uri, width: 255, height: 255}} style={{borderRadius:15}} /> 
             </View>  
-            
             <Text style={styles.historyContainer}>{searchQuery}</Text>
-            <Button title="Generate a similair image" />
-            <Button title="Remove from history" onPress={() => onRemove ? onRemove(id) : () => {}} />
+            
+            
+            <Pressable style={styles.button}><Text style={styles.buttonText}>Generate a similair image</Text></Pressable>
+            <Pressable style={styles.button} onPress={() => onRemove ? onRemove(id) : () => {}}><Text style={styles.buttonText}>Remove from history</Text></Pressable>
         </View>
     )   
 }
